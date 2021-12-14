720f9f278baa93cb693d2d968874899a8034eda0
yupliu <yupliu@users.noreply.github.com>
2021-12-13 23:03:40 -0500

    main

--- Logistic_regression.py
+++ Logistic_regression.py
@@ -4,4 +4,13 @@ import h5py
 import scipy
 from PIL import Image
 from scipy import ndimage
+from sklearn.datasets import load_iris
+from sklearn.linear_model import Perceptron
 
+iris = load_iris()
+x = iris.data[:,(2,3)]
+y = (iris.target == 0).astype(np.int32)
+pc = Perceptron()
+pc.fit(x,y)
+y_pred = pc.predict([[2,0.5]])
+print(y_pred)
