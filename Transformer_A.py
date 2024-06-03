7fd412f5fb078dce81375005e07d4e80a1cdcba0
yupliu <yupliu@users.noreply.github.com>
2024-06-03 12:01:50 -0400

    Transformer

---
+++ Transformer_A.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import math
+import copy
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self,d_model,num_heads):
+        super(MultiHeadAttention,self).__init__()
+        assert d_model % num_heads == 0, "d_model must be divisible by num_heads"
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.d_k = d_model
+
+        self.W_q = nn.Linear(d_model,d_model)
+        self.W_k = nn.Linear(d_model,d_model)
+        self.W_v = nn.Linear(d_model,d_model)
+        self.W_o = nn.Linear(d_model,d_model)
+
+    def scaled_dot_product_attention(self,Q,K,V, mask=None):
+        
